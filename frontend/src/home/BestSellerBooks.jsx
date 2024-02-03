2ef5423a976ac5452ec633ecce1c7730a2491f82
sourya911 <97163620+sourya911@users.noreply.github.com>
2024-02-04 00:18:41 +0530

    Update BestSellerBooks.jsx

--- frontend/src/home/BestSellerBooks.jsx
+++ frontend/src/home/BestSellerBooks.jsx
@@ -5,7 +5,7 @@ const BestSellerBooks = () => {
     const[books,setBooks]=useState([]);
 
    useEffect(() => {
-    fetch("https://book-store-s.vercel.app/shop")
+    fetch("https://book-store-s.vercel.app/books")
       .then((res) => res.json())
       .then((data) => setBooks(data));
   });
