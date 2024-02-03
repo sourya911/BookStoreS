0392070626aa26d2a936d31823485ab2c5fbe48a
sourya911 <97163620+sourya911@users.noreply.github.com>
2024-02-03 23:32:54 +0530

    Update BestSellerBooks.jsx

--- frontend/src/home/BestSellerBooks.jsx
+++ frontend/src/home/BestSellerBooks.jsx
@@ -5,7 +5,7 @@ const BestSellerBooks = () => {
     const[books,setBooks]=useState([]);
 
     useEffect(()=>{
-        fetch('https://book-store-s-front.vercel.app').then(res=>res.json().then(data=>setBooks(data)))
+        fetch('https://book-store-s.vercel.app/books').then(res=>res.json().then(data=>setBooks(data)))
     },[])
   return (
     <div>
